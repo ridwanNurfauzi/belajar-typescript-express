01a3cd0e62e9f848bb98a59a094eefaa8957430b
ridwanNurfauzi <mrnhp123@gmail.com>
2024-01-06 20:30:17 +0700

    fix timezone

--- src/config/dbConnect.ts
+++ src/config/dbConnect.ts
@@ -1,5 +1,6 @@
 import { Dialect, Sequelize } from "sequelize";
 import dotenv from "dotenv";
+import tzList from "timezones-list";
 
 dotenv.config();
 
@@ -10,12 +11,14 @@ const dbUsername = String(process.env.DB_USERNAME);
 const dbPassword = String(process.env.DB_PASSWORD);
 const dbDialect = process.env.DB_DIALECT;
 
+const timezone = tzList.find(e => e.tzCode == (process.env.TZ || 'Asia/Jakarta'))?.utc;
+
 const sequelizeConnection = new Sequelize(dbName, dbUsername, dbPassword, {
     host: dbHost,
     port: dbPort,
     dialect: dbDialect as Dialect,
     logging: false,
-    timezone: process.env.TZ || 'Asia/Jakarta'
+    timezone
 });
 
 export default sequelizeConnection;
