e2eaf78c36d522e549041d776d280ac6ada7ef97
ridwanNurfauzi <mrnhp123@gmail.com>
2024-01-06 19:31:31 +0700

    added method-override

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
+import methodOverride from "method-override";
 import server from "./config/server";
 import router from "./routes";
 
@@ -7,6 +8,15 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use(methodOverride((req: Request, res: Response) => {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        var method = req.body._method;
+
+        delete req.body._method;
+        return method;
+    }
+}));
 
 app.use(router);
 
